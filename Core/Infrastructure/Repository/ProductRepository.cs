efb5550f9df51fe30132588c55b993827f631c16
HaileyesusB <HBelew@excellerentsolutions.com>
2021-10-08 11:40:09 +0300

    Infrastructure Dll

---
+++ Core/Infrastructure/Repository/ProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repository
+{
+    public class ProductRepository: IProductRepository
+    {
+        public Task<Products> GetProductByIDAsync(int Id) {
+            throw new System.Exception();
+        }
+
+        public Task<IReadOnlyList<Products>> GetProductAsync()
+        {
+            throw new System.Exception();
+        }
+    }
+}
